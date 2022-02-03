39a9a76ffc7887bf3c5c61e15bc3f9402f0c7efe
Pobarulislam <pobarul.islam10@gmail.com>
2022-02-03 10:06:52 +0600

    apply js concepts file added

---
+++ recap.js
@@ -0,0 +1,49 @@
+// variable 
+var time = '10:50';
+var bookPrice = 150;
+var isWhitecolor = false;
+
+
+// array 
+var partners = ['sajid', 'rajib', 'abul', 'kabul', 'sabul'];
+var elemmentCount = partners.length;
+var rajibIndex = partners.indexOf('rajib'; partners.push('kabul'); partners.pop();
+
+
+        // conditionals
+
+        if (bookPrice < 200) {
+            console.log('I will buy this book');
+        } else {
+            console.log('mama , kichu discount din na, apni na mama')
+        }
+
+        // Loop
+
+        var i = 0;
+        while (i <= 17) {
+            i++;
+        }
+        for (var i = 0; i <= 17; i++) {
+
+        }
+
+        // function 
+        function isMoonUp() {
+            if (time >= 19 && time <= h5) {
+                return false;
+            }
+            return true;
+        }
+
+        var MoonStatus = isMoonUp(21);
+
+
+        // let const
+
+        // value of the variable could chage 
+        let price = 27; price = 20; price = 30;
+
+        // value of the variable will not change
+
+        const myName = 'lal e lal mr. Helal'; console.log(myName); myName = 22;
